bf78ce12be7e7f22b2c050d30b738dd2c42dadb8
jemohl <jemohl@utep.edu>
2020-02-25 09:17:16 -0700

    Fixed trimming and naming of files in initial steps

--- process_sequences.py
+++ process_sequences.py
@@ -18,6 +18,7 @@ parser.add_argument('-b', '--bait_reference', metavar="FILENAME", dest='bait_ref
 parser.add_argument('-o', '--output_dir', metavar="DIRNAME", dest='outdir', required=True, help='Required: Name of output directory to create.')
 parser.add_argument('-a', '--ancient_list', metavar="FILENAME", dest='anc_list', help='This file contains the names of the files that may need the MapDamage correction')
 parser.add_argument('-t', '--threads', metavar="INT", dest='threads', type=int, default=1)
+parser.add_argument('-PE', '--PairedEnd', dest='PE', action='store_true',help='Is used for PE fastq files')
 args = parser.parse_args()
 
 raw = args.raw
@@ -25,6 +26,10 @@ bait_ref = args.bait_ref
 outdir = args.outdir
 threads = args.threads
 ancient = args.anc_list
+if args.PE:
+   PE = True
+else:
+   PE = False
 
 #Check to make sure directory is not there
 if os.path.exists(outdir):
@@ -45,20 +50,31 @@ mapdamage_log = open('%s/bait_run_log.txt'%outdir,'w')
 print('Starting to process sequences.')
 
 file_names = os.listdir(raw)
+print(file_names)
 acc = []
 for fn in file_names:
    if fn[0] != '.':
-      if 'fastq' in fn.split('.'):
-         acc.append(fn.split('.')[0])
-         comm = '%s SE -threads %s -phred33 %s/%s %s/trimmed/%s.trimmed.fastq.gz SLIDINGWINDOW:5:30 MINLEN:30'%(TRIM,threads,raw,fn,outdir,fn.split('.')[0])
-      else:
-         new_acc = fn[0:-3]
-         acc.append(new_acc)
-         comm = '%s SE -threads %s -phred33 %s/%s %s/trimmed/%s.trimmed.fastq.gz SLIDINGWINDOW:5:30 MINLEN:30'%(TRIM,threads,raw,fn,outdir,new_acc)
-      process = subprocess.Popen(comm, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-      process.wait()
-      mapdamage_log.write(''.join(process.stdout.readlines()))
-      mapdamage_log.write(''.join(process.stderr.readlines()))
+      print(fn)
+      if PE and ('_R1' in fn):
+         acc.append(fn.split('_R1')[0])
+         fn2 = fn.split('_R1')[0]+'_R2.fastq.gz'
+         comm = '%s PE -threads %s -phred33 %s/%s %s/%s %s/trimmed/%s_R1.tp.fastq.gz %s/trimmed/%s_R1.tu.fastq.gz %s/trimmed/%s_R2.tp.fastq.gz %s/trimmed/%s_R2.tu.fastq.gz SLIDINGWINDOW:5:30 MINLEN:30'%(TRIM,threads,raw,fn,raw,fn2,outdir,fn.split('_R1')[0],outdir,fn.split('_R1')[0],outdir,fn.split('_R1')[0],outdir,fn.split('_R1')[0])
+         process = subprocess.Popen(comm, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+         process.wait()
+         mapdamage_log.write(''.join(process.stdout.readlines()))
+         mapdamage_log.write(''.join(process.stderr.readlines()))
+      elif not PE:
+         if 'fastq' in fn.split('.'):
+            acc.append(fn.split('.')[0])
+            comm = '%s SE -threads %s -phred33 %s/%s %s/trimmed/%s.trimmed.fastq.gz SLIDINGWINDOW:5:30 MINLEN:30'%(TRIM,threads,raw,fn,outdir,fn.split('.')[0])
+         else:
+            new_acc = fn[0:-3]
+            acc.append(new_acc)
+            comm = '%s SE -threads %s -phred33 %s/%s %s/trimmed/%s.trimmed.fastq.gz SLIDINGWINDOW:5:30 MINLEN:30'%(TRIM,threads,raw,fn,outdir,new_acc)
+         process = subprocess.Popen(comm, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+         process.wait()
+         mapdamage_log.write(''.join(process.stdout.readlines()))
+         mapdamage_log.write(''.join(process.stderr.readlines()))
 
 print('There are %s files to process.'%len(acc))
 
@@ -83,7 +99,10 @@ for a in acc:
    print('Processing %s of %s files.'%(i,len(acc)))
    mapdamage_log.write('Processing %s of %s files.'%(i,len(acc)))
 
-   comm = '%s mem -t %s %s -T 30 -I 1024 -w O %s/trimmed/%s.trimmed.fastq.gz > %s/aligned/%s.bwa.sam'%(BWA,threads,bait_ref,outdir,a,outdir,a)
+   if PE:
+      comm = '%s mem -t %s %s -T 30 -I 1024 -w O %s/trimmed/%s_R1.tp.fastq.gz %s/trimmed/%s_R2.tp.fastq.gz> %s/aligned/%s.bwa.sam'%(BWA,threads,bait_ref,outdir,a,outdir,a,outdir,a)
+   else:
+      comm = '%s mem -t %s %s -T 30 -I 1024 -w O %s/trimmed/%s.trimmed.fastq.gz > %s/aligned/%s.bwa.sam'%(BWA,threads,bait_ref,outdir,a,outdir,a)
    process = subprocess.Popen(comm, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
    process.wait()
    mapdamage_log.write('bwa command: %s\n'%comm)
